a6a3ec1d964861a745f4083e193ddc5020e74dec
vasu lakkoju <vasulakkoju313@gmail.com>
2025-06-08 19:41:27 +0530

    fix the all files

--- auth/settings.py
+++ auth/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 from decouple import config
 import dj_database_url
 
