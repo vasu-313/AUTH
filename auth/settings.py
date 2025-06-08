710c5b402390484d11f9b00614ba88d4c72db643
vasu lakkoju <vasulakkoju313@gmail.com>
2025-06-08 22:47:31 +0530

    update new files

--- auth/settings.py
+++ auth/settings.py
@@ -117,12 +117,11 @@ DATABASES = {
     }
 }
 
-
+print("DATABASE_URL =", repr(os.environ.get('DATABASE_URL')))
 
 DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
